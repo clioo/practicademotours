2eb08ed05767a62264402aceed97751ae70437eb
jesus_acosta1996@hotmail.com <you@example.com>
2019-10-04 12:42:30 -0700

    asdasdsadasd

--- src/test/java/org/softtek/pageObjects/FindFlightPage.java
+++ src/test/java/org/softtek/pageObjects/FindFlightPage.java
@@ -30,6 +30,51 @@ public class FindFlightPage {
 		select.selectByVisibleText(value);
 	}
 	
+	public void selectDpartingFrom(String value) {
+		Select select = new Select(flightFinderContent.selectDepartingFrom);
+		select.selectByVisibleText(value);
+	}
+	
+	public void selectOnMonth(String value) {
+		Select select = new Select(flightFinderContent.selectOnMonth);
+		select.selectByVisibleText(value);
+	}
+	
+	public void selectOnDay(String value) {
+		Select select = new Select(flightFinderContent.selectOnMonth);
+		select.selectByVisibleText(value);
+	}
+	
+	public void selectArriving(String value) {
+		Select select = new Select(flightFinderContent.selectArriving);
+		select.selectByVisibleText(value);
+	}
+	
+	public void selectReturningMonth(String value) {
+		Select select = new Select(flightFinderContent.selectReturningMonth);
+		select.selectByVisibleText(value);
+	}
+	
+	public void selectReturningDay(String value) {
+		Select select = new Select(flightFinderContent.selectReturningDay);
+		select.selectByVisibleText(value);
+	}
+	
+	
+	public void selectRadioService(String type){
+		if(type.equals("Economy class")) {
+			flightFinderContent.radioServiceEconomy.click();
+		}else if(type.equals("Business class")) {
+			flightFinderContent.radioServiceBusiness.click();
+		}else if(type.equals("First class")) {
+			flightFinderContent.radioServiceFirstClass.click();
+		}
+	}
+	
+	public void submit(String value) {
+		flightFinderContent.findFlightsButton.click();
+	}
+	
 	
 	
 }
