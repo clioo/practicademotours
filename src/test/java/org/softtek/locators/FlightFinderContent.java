eb9171ebabcc66c3ed27a47b858cbb2b8a6841c7
jesus_acosta1996@hotmail.com <you@example.com>
2019-10-04 12:25:14 -0700

    hijo de tu puta madrte

---
+++ src/test/java/org/softtek/locators/FlightFinderContent.java
@@ -0,0 +1,47 @@
+package org.softtek.locators;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+public class FlightFinderContent {
+	@FindBy(xpath= "//img[@src='/images/masts/mast_flightfinder.gif']")
+	public WebElement imageFlightFinder; //used to validate the page is shown
+	
+	@FindBy(xpath= "//input[@value='roundtrip']")
+	public WebElement radioTripTypeRound;
+	
+	@FindBy(xpath= "//input[@value='oneway']")
+	public WebElement radioTripTypeOneWay;
+	
+	@FindBy(xpath= "//select[@name='passCount']")
+	public WebElement selectPassengers;
+	
+	@FindBy(xpath= "//select[@name='fromPort']")
+	public WebElement selectDepartingFrom;
+
+	@FindBy(xpath= "//select[@name='fromMonth']")
+	public WebElement selectOnMonth;
+	
+	@FindBy(xpath= "//select[@name='fromDay']")
+	public WebElement selectOnDay;
+	
+	@FindBy(xpath= "//select[@name='toPort']")
+	public WebElement selectArriving;
+	
+	@FindBy(xpath= "//select[@name='toMonth']")
+	public WebElement selectReturningMonth;
+	
+	@FindBy(xpath= "//select[@name='toDay']")
+	public WebElement selectReturningDay;
+	
+	@FindBy(xpath= "//input[@value='Coach']")
+	public WebElement radioServiceEconomy;
+
+	@FindBy(xpath= "//input[@value='Business']")
+	public WebElement radioServiceBusiness;
+	
+	@FindBy(xpath= "//input[@value='First']")
+	public WebElement radioServiceFirstClass;
+	
+	@FindBy(xpath= "//select[@name='airline']")
+	public WebElement selectAirline;
+	
+}
--- src/test/java/org/softtek/tests/test.java
+++ src/test/java/org/softtek/tests/test.java
@@ -5,6 +5,8 @@ import static org.testng.Assert.assertEquals;
 
 
 import org.softtek.config.Setup;
+import org.softtek.objects.User;
+import org.softtek.pageObjects.HomePage;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -18,9 +20,13 @@ public class test extends Setup{
 		setBrowser("Internet explorer");
 	}
 	
-	@Test(dataProvider = "DataGoogleSheets", description="Test Case for Register an user")
+	@Test(dataProvider = "DataGoogleSheets", description="I want to login")
     public void prueba1(String ... data) {
-		System.out.println(data[0]);
+		User user = new User(data[0],data[1]); // 0 = username | 1 = password
+		HomePage homePage = new HomePage(driver);
+		homePage.setUsername(user.getUsername());
+		homePage.setPassword(user.getPassword());
+		homePage.login();
     }
     
 	@DataProvider(name = "DataGoogleSheets")
